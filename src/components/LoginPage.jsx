222f7d033dc9024c7433e17f58f3fa9011b68ff1
Surendra <surendrasaluru@gmail.com>
2024-11-04 20:11:15 +0530

    installed axios and cors and also added login page in UI.

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,5 +1,69 @@
+import axios from "axios";
+import { useState } from "react";
+
 const LoginPage = () => {
-  return <div className="text-white">LoginPage</div>;
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      await axios.post(
+        "http://localhost:3000/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+      console.log("login is fine at UI");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card bg-base-500 w-96 shadow-xl">
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">LOGIN</h2>
+          <div>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">EMAIL ID</span>
+              </div>
+              <input
+                type="text"
+                value={emailId}
+                className="input input-bordered w-full max-w-80"
+                onChange={(e) => {
+                  setEmailId(e.target.value);
+                }}
+              />
+            </label>
+            <label className="form-control w-full max-w-xs my-2">
+              <div className="label">
+                <span className="label-text">PASSWORD</span>
+              </div>
+              <input
+                type="text"
+                value={password}
+                className="input input-bordered w-full max-w-80"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </label>
+          </div>
+
+          <div className="card-actions justify-center">
+            <button className="btn btn-success" onClick={handleLogin}>
+              LOGIN
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
